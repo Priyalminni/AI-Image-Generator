45f0cab4606367643a2ca0b1090881abdf121d65
Priyalminni <minnipriyal12@gmail.com>
2024-05-07 18:14:52 +0530

    Committed code with new UI

--- client/main.js
+++ client/main.js
@@ -1,18 +1,22 @@
 const generateBtn = document.querySelector(".generate-btn");
 const promptInput = document.querySelector(".prompt-input");
-const generatedImage = document.querySelector(".generated-image");
+const generatedImage1 = document.querySelector(".generated-image1");
+const generatedImage2 = document.querySelector(".generated-image2");
+const generatedImage3 = document.querySelector(".generated-image3");
 const downloadBtn = document.querySelector(".download-btn");
 
 const showNotification = (message) => {
   alert(message);
 };
 
-const generateImage = async () => {
+const genImage = async () => {
   const prompt = promptInput.value;
 
   if (prompt) {
     try {
-      generatedImage.src = "images/generating.png";
+      generatedImage1.src = "images/generating.png";
+      generatedImage2.src = "images/generating.png";
+      generatedImage3.src = "images/generating.png";
 
       const response = await fetch("http://localhost:4000/generateImage", {
         method: "POST",
@@ -23,17 +27,26 @@ const generateImage = async () => {
       });
 
       const data = await response.json();
-      const image = `data:image/jpeg;base64,${data.image}`;
-      generatedImage.src = image;
-      downloadBtn.href = image;
+      // const base64Image1 = data.images[0];
+      // const base64Image2 = data.images[1];
+      // const base64Image3 = data.images[2];
+      generatedImage1.src = `data:image/png;base64,${data.images[0]}`;
+      generatedImage2.src = `data:image/png;base64,${data.images[1]}`;
+      generatedImage3.src = `data:image/png;base64,${data.images[2]}`;
+
+      downloadBtn.href = `data:image/png;base64,${data.images[0]}`;
+      downloadBtn.href = `data:image/png;base64,${data.images[1]}`;
+      downloadBtn.href = `data:image/png;base64,${data.images[2]}`;
     } catch (error) {
       console.log(error);
       showNotification("The image could not be generated.");
-      generatedImage.src = "images/image-placeholder.png";
+      generatedImage1.src = "images/image-placeholder1.png";
+      generatedImage2.src = "images/image-placeholder2.png";
+      generatedImage3.src = "images/image-placeholder3.png";
     }
   } else {
     showNotification("Please enter the prompt");
   }
 };
 
-generateBtn.addEventListener("click", generateImage);
+generateBtn.addEventListener("click", genImage);
--- server/server.js
+++ server/server.js
@@ -12,21 +12,23 @@ app.listen(4000);
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAIApi(configuration);//instance where we can send request 
 
 const generateImage = async (prompt) => {
   const response = await openai.createImage({
     prompt: prompt,
-    n: 1,
-    size: "1024x1024",
+    n: 3,
+    size: "512x512",
     response_format: "b64_json",
-  });
-
-  const image = response.data.data[0].b64_json;
-  return image;
+  });// resquest to call openai,resquest payload
+//512x512 or 256x256
+  const image1 = response.data.data[0].b64_json;
+  const image2 = response.data.data[1].b64_json;
+  const image3 = response.data.data[2].b64_json;
+  return [image1, image2, image3];
 };
 
 app.post("/generateImage", async (req, res) => {
-  const image = await generateImage(req.body.prompt);
-  res.send({ image });
+  const images = await generateImage(req.body.prompt);
+  res.send({ images });
 });

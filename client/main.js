9d0a4e8d2d6283ed73a15f57998f051710a4b409
Priyalminni <minnipriyal12@gmail.com>
2024-05-07 20:41:03 +0530

    committed render webservice path

--- client/main.js
+++ client/main.js
@@ -18,7 +18,7 @@ const genImage = async () => {
       generatedImage2.src = "images/generating.png";
       generatedImage3.src = "images/generating.png";
 
-      const response = await fetch("http://localhost:4000/generateImage", {
+      const response = await fetch("https://ai-image-generator-m3ww.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
